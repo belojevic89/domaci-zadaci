1708dcfcafe995a05f53ace22818a6ac6c4edbb0
belojevic89 <belojevicbojana@gmail.com>
2019-11-24 23:26:44 +0100

    Adds third homework

---
+++ domaci3/kvadar.php
@@ -0,0 +1,13 @@
+
+
+<?php 
+ 
+  $a=10;
+  $b=13;
+  $c=14;
+
+  echo "Povrsina kvadra : " . 2*($a*$b + $a*$c + $b*$c). PHP_EOL . "<br/> <br/>";
+
+  echo "Zapremina kvadra : " . $a*$b*$c . PHP_EOL ;
+
+  ?>
